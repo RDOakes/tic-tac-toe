c5102fc01c183e3453d3f1bc5f18e0bc6e733059
Ryan <98277569+RDOakes@users.noreply.github.com>
2023-02-08 15:54:52 -0500

    nowhere near

---
+++ script.js
@@ -0,0 +1,17 @@
+const playerOne = () => {
+    document.getElementsByClassName('button').setAttribute("id", "red");
+}
+const playerTwo = () => {
+    document.getElementsByClassName('button').setAttribute("id", "yellow");
+}
+/*
+const takeTurn = (turn) => {
+    if (turn === 'playerOne') {
+        turn = 'playerTwo';
+    } else {
+        turn = 'playerOne';
+    }
+    for (turn = playerOne, )
+}*/
+
+for (let i = 0; i < 2; i++)
